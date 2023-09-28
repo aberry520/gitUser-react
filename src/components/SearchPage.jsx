58348e6e40afbf1da910b1a1537d75a7e683b616
Austin Berry <aberry52098@gmail.com>
2023-09-28 14:25:25 -0400

    added styles

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 import { UserDisplay } from "./UserDisplay";
 import { UsernameForm } from "./UsernameForm";
+import styled from "styled-components";
 
+const CardDiv = styled.div `
+    /* border: solid 2px pink; */
+    /* width: 100vw; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    max-width: 100vw;
+    height: 100vh;
+    margin: 0;
+`;
+const NavBar = styled.div`
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: center;
+`;
 export const SearchPage = () => {
     const [userImage, setUserImage] = useState("https://bulma.io/images/placeholders/96x96.png");
     const [usernameInput, setUsernameInput] = useState();
@@ -29,15 +47,13 @@ export const SearchPage = () => {
         if (data.message) {
             setUserName("User Not Found");
             setUserImage("https://bulma.io/images/placeholders/96x96.png");
-            setLogin("Not Found");
+            setLogin(data.message);
             setLocation("Not Found");
             setBio("Not Found");
             setRepos("0");
             setFollowers("0");
 
         }
-
-
         return data;
     };
 
@@ -50,9 +66,13 @@ export const SearchPage = () => {
 
     };
     return (
-        <>
-            <UsernameForm search={search} handleChange={handleChange} usernameInput={usernameInput} />
+        <>     
+        <CardDiv>
+        <NavBar>
+        <UsernameForm search={search} handleChange={handleChange} usernameInput={usernameInput} />
+        </NavBar> 
             <UserDisplay userImage={userImage} userName={userName} login={login} location={location} bio={bio} repos={repos} followers={followers} />
+        </CardDiv>
         </>
     )
 };
--- src/components/UsernameForm.jsx
+++ src/components/UsernameForm.jsx
@@ -1,12 +1,80 @@
-export const UsernameForm = ({search, handleChange, usernameInput})=>{
-   
+import { useState } from "react";
+
+export const UsernameForm = ({ search, handleChange, usernameInput }) => {
+    const [value, setValue] = useState("");
+    const handleChildChange = (e) => {
+        setValue(e.target.value);
+        handleChange(e);
+    };
+
     return (
         <>
-        <form>
-            <input type="text" placeholder="enter username" value={usernameInput} onChange={(e) => { handleChange(e) }}/>
-            <button type="button" onClick={()=>search()}>Search</button>
-        </form>
-            
+            <nav className="navbar" style={{width: "100vw", fontSize: "10px"}} role="navigation" aria-label="main navigation">
+                <div className="navbar-brand" style={{alignItems:"center"}}>
+                    <a className="navbar-item" href="https://bulma.io">
+                        <img src="../public/images/git-icon.webp" width="auto" height="28" />
+                    </a>
+                    <form>
+                        <input type="text" placeholder="enter username" className="iput" value={value} onChange={(e) => { handleChildChange(e) }} />
+                        <button type="button" className="buton" onClick={() => search()}>Search</button>
+                    </form>
+                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a className="navbar-item">
+                            Home
+                        </a>
+
+                        <a className="navbar-item">
+                            Documentation
+                        </a>
+
+                        <div className="navbar-item has-dropdown is-hoverable">
+                            <a className="navbar-link">
+                                More
+                            </a>
+
+                            <div className="navbar-dropdown">
+                                <a className="navbar-item">
+                                    About
+                                </a>
+                                <a className="navbar-item">
+                                    Jobs
+                                </a>
+                                <a className="navbar-item">
+                                    Contact
+                                </a>
+                                <hr className="navbar-divider" />
+                                <a className="navbar-item">
+                                    Report an issue
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <a className="button is-primary">
+                                    <strong>Sign up</strong>
+                                </a>
+                                <a className="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+
         </>
     )
 };
